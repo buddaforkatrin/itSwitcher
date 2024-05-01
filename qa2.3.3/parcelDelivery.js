1adbe7b12c10c93ae988c2d890e85f3cee23c56f
buddaforkatrin <buddaforkatrin@gmail.com>
2024-05-01 21:01:06 +0200

    hw 2.3.3 parcel delivery function added

---
+++ qa2.3.3/parcelDelivery.js
@@ -0,0 +1,22 @@
+let userBalance = 500;
+const userParcelPrice = 100;
+const parcelLocker = [null, null, null, "1432HGF", null];
+const userSmsCode = "A001DFX0";
+const BD = [{ id: "dsdsd", price: 900 }];
+
+function getMessage(code) {
+  if (parcelLocker.indexOf(code) != -1) {
+    userBalance -= userParcelPrice;
+    console.log(
+      `Заберите посылку ${code} из ячейки ${parcelLocker.indexOf(
+        code
+      )}. Ваш счет составляет: ${userBalance}ед.`
+    );
+  } else {
+    console.log(
+      "Посылка с данным кодом не найдена. Проверьте правильность введенного кода или обратитесь в нашу техническую поддержку."
+    );
+  }
+}
+
+getMessage(userSmsCode);
